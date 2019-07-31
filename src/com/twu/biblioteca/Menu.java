b1177f42967f4baf3a9c214ddafc1d2d02b9e44e
Clare <clare.pinder@live.co.uk>
2019-07-31 17:06:43 +0100

    Passing test for Menu class, shouldCheckOutBookWhenOption2IsSelected

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -1,9 +1,14 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class Menu {
     private LibraryInventory libraryInventory;
 
-    static String menuOptions = "\nBibliotecaApp menu - please select a number.\n1. List of books";
+    static String menuOptions = "\nBibliotecaApp menu - please select a number." +
+            "\n1. List of books" +
+            "\n2. Check out book" +
+            "\n4. Quit";
 
     public Menu(LibraryInventory libraryInventory) {
         this.libraryInventory = libraryInventory;
@@ -17,6 +22,11 @@ public class Menu {
         String menuAction = "";
         if (menuChoice == 1) {
             menuAction = libraryInventory.getAvailableBooks();
+        } else if (menuChoice == 2) {
+            System.out.println("Pls type in the book reference e.g. 2");
+            Scanner userInput = new Scanner(System.in);
+            int bookRef = userInput.nextInt();
+            menuAction = libraryInventory.checkOutBook(bookRef);
         } else if (menuChoice == 4) {
             System.exit(0);
         }
--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -1,10 +1,14 @@
 package com.twu.biblioteca;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 import org.mockito.Mock;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import static com.sun.javaws.JnlpxArgs.verify;
 import static org.hamcrest.CoreMatchers.is;
@@ -13,6 +17,8 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class MenuTest {
+    private final InputStream systemIn = System.in;
+    private ByteArrayInputStream testIn;
     LibraryInventory libraryInventory;
     Menu menu;
 
@@ -22,28 +28,29 @@ public class MenuTest {
     @Before
     public void setUp() {
         libraryInventory = new LibraryInventory();
-    }
-
-    @Before
-    public void setUp2() {
         menu = new Menu(libraryInventory);
-    }
-
-
-    @Test
-    public void shouldGetMenuOptions() {
-        LibraryInventory libraryInventory = new LibraryInventory();
-        Menu menu = new Menu(libraryInventory);
-        String menuOptions = "\nBibliotecaApp menu - please select a number.\n1. List of books";
-        assertThat(menu.getMenuOptions(), is (menuOptions));
-    }
-
-    @Test
-    public void shouldGetAvailableBooksWhenOption1IsSelected() {
         Book book = new Book(1, "testTitle", "testAuthor", 1990);
         Book book2 = new Book(2, "testTitle", "testAuthor", 1990);
         libraryInventory.addBook(book);
         libraryInventory.addBook(book2);
+    }
+
+    @After
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+    }
+
+    @Test
+    public void shouldGetMenuOptions() {
+        String menuOptions = "\nBibliotecaApp menu - please select a number." +
+                "\n1. List of books" +
+                "\n2. Check out book" +
+                "\n4. Quit";
+        assertThat(menu.getMenuOptions(), is(menuOptions));
+    }
+
+    @Test
+    public void shouldGetAvailableBooksWhenOption1IsSelected() {
         String availableBooks = "1, testTitle, testAuthor, 1990\n2, testTitle, testAuthor, 1990";
         assertThat(menu.selectMenuOption(1), is(availableBooks));
     }
@@ -52,6 +59,13 @@ public class MenuTest {
     public void shouldQuitWhenOption4IsSelected() {
         exit.expectSystemExitWithStatus(0);
         menu.selectMenuOption(4);
+    }
 
+    @Test
+    public void shouldCheckOutBookWhenOption2IsSelected() {
+        String userInput = "1";
+        testIn = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(testIn);
+        assertThat(menu.selectMenuOption(2), is("Thank you! Enjoy the book"));
     }
 }
