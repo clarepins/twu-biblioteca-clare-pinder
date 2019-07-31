aa7212dcb0d78281ca8798ab5bd07485b48e984b
Clare <clare.pinder@live.co.uk>
2019-07-31 16:11:26 +0100

    Passing test for LibraryInventory class, shouldCheckOutBook

--- src/com/twu/biblioteca/LibraryInventory.java
+++ src/com/twu/biblioteca/LibraryInventory.java
@@ -1,6 +1,7 @@
 package com.twu.biblioteca;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class LibraryInventory {
     ArrayList<Book> availableBooks = new ArrayList();
@@ -16,5 +17,19 @@ public class LibraryInventory {
         ArrayList<String> bookInfoList = new ArrayList();
         availableBooks.forEach(book -> bookInfoList.add(book.getBookInfo()));
         return String.join("\n", bookInfoList);
+    }
+
+    public String checkOutBook(int bookRef) { // had to use weird Iterator syntax to avoid ConcurrentModificationException
+        Iterator<Book> iterator = availableBooks.iterator();
+        String checkOutMessage = "";
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+
+            if (book.getRef() == bookRef) { // maybe build a method that takes listToAddTo and listToRemoveFrom
+                iterator.remove();
+                checkOutMessage = "Thank you! Enjoy the book";
+            }
+        }
+        return checkOutMessage;
     }
 }
--- test/com/twu/biblioteca/LibraryInventoryTest.java
+++ test/com/twu/biblioteca/LibraryInventoryTest.java
@@ -1,21 +1,37 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class LibraryInventoryTest {
+    Book book;
+    Book book2;
+    LibraryInventory libraryInventory;
+
+    @Before
+        public void setUp() {
+            book = new Book(1, "title1", "author1", 1990);
+            book2 = new Book(2, "title2", "author2", 1990);
+            libraryInventory = new LibraryInventory();
+            libraryInventory.addBook(book);
+            libraryInventory.addBook(book2);
+        }
+
 
     @Test
     public void shouldAddBooksToInventory() {
-        Book book = new Book(1, "title1", "author1", 1990);
-        Book book2 = new Book(2, "title2", "author2", 1990);
-        LibraryInventory libraryInventory = new LibraryInventory();
-        libraryInventory.addBook(book);
-        libraryInventory.addBook(book2);
         String bookList = "1, title1, author1, 1990\n2, title2, author2, 1990";
+        assertThat(libraryInventory.getAvailableBooks(), is(bookList));
+    }
+
+    @Test
+    public void shouldCheckoutBook() {
+        libraryInventory.checkOutBook(1);
+        String bookList = "2, title2, author2, 1990";
         assertThat(libraryInventory.getAvailableBooks(), is(bookList));
     }
 
